387da06e62f058fddf963d55b83d5209cf181824
dragosvasile11 <vasile.dragos11@gmail.com>
2022-09-19 10:59:24 +0300

    Add ExceptionsMessages.

---
+++ src/main/java/utils/ExceptionsMessages.java
@@ -0,0 +1,11 @@
+package utils;
+
+/***
+ * This class is used to store messages for different exceptions that may occur.
+ */
+public class ExceptionsMessages {
+    public static final String RUNTIME_EXCEPTION_MESSAGE = "Operation interrupted or failed: ";
+    public static final String FILE_NOT_FOUND_MESSAGE = "File was not found as specified path: ";
+    public static final String NULL_MESSAGE = "File was not detected by watchDirectory().";
+}
+
